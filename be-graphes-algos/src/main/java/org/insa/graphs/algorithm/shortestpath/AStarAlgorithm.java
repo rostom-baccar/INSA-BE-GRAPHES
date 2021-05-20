3cb49f0c941a927554a5ca8292df03badcf3bd95
98rostom <81230614+98rostom@users.noreply.github.com>
2021-05-20 16:39:37 +0200

    update AStarAlgorithm

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/AStarAlgorithm.java
@@ -18,24 +18,25 @@ public class AStarAlgorithm extends DijkstraAlgorithm {
  	   LabelStar labels[] = new LabelStar[nbr_noeuds] ;
  	   List<Node> nodes = graphe.getNodes();
  	   
- 	   double Cost = 0;
-
- 	   //int MaxSpeed = Speed() ; 
+ 	   double coût = 0;
  	   
- 	   Point DestinationP = data.getDestination().getPoint() ; 
+ 	   //point de destination
+ 	   Point point_destination = data.getDestination().getPoint() ; 
  	   
- 	   for (Node node : nodes) {
- 		  labels[node.getId()] = new LabelStar(node);
+ 	   //création des labels star
+ 	   for (Node noeud : nodes) {
+ 		  labels[noeud.getId()] = new LabelStar(noeud);
  		   
- 		   // the cost is the distance between this point and the destination point, in meters
+ 		  //si le mode sélectionné est LENGTH, on calcule le coût en question en calculant
+ 		  //la longueur entre le noeud courant et la destination
  		   if(data.getMode() == AbstractInputData.Mode.LENGTH) {
- 			   Cost = node.getPoint().distanceTo(DestinationP);
+ 			  coût = noeud.getPoint().distanceTo(point_destination);
  			   
- 			   //or it's the time (ie Distance divided by speed) 
+ 		//si c'est le temps le plus court qui nous intéresse, on divise cette longueur par la vitesse 
  	       	} else {
- 	       		Cost = node.getPoint().distanceTo(DestinationP) / data.getMaximumSpeed(); 
+ 	       	coût = noeud.getPoint().distanceTo(point_destination) / data.getMaximumSpeed(); 
  	       	}
- 		  labels[node.getId()].set_vol_oiseau(Cost);
+ 		  labels[noeud.getId()].set_vol_oiseau(coût);
  	   }
  	   return labels ; 
      }
